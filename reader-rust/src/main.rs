53b8ba0cab4df9dbec4b648d92e7cfd37fc6d707
darkstorm <john.kalmykov@gmail.com>
2024-01-28 15:07:45 +0100

    bug-fix

--- reader-rust/src/main.rs
+++ reader-rust/src/main.rs
@@ -4,7 +4,7 @@ const LOGDIR: &str = "../";
 
 fn main() {
     println!("Reading keys input...");
-    let log = fs::read_to_string(LOGDIR).unwrap().lines();
+    let log = fs::read_to_string(LOGDIR).expect("Should have been able to read the file");;
     println!("{log}");
 
 }
