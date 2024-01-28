27ce0325a3f423f956a260ee9de9e4866cd28674
darkstorm <john.kalmykov@gmail.com>
2024-01-28 15:06:38 +0100

    test-rust

--- reader-rust/src/main.rs
+++ reader-rust/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+
+const LOGDIR: &str = "../";
+
 fn main() {
     println!("Reading keys input...");
+    let log = fs::read_to_string(LOGDIR).unwrap().lines();
+    println!("{log}");
+
 }
